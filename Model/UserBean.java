71de9538fb80f3edffe7e96e88720dfee3a68307
MinThwayKhaing <110376802+MinThwayKhaing@users.noreply.github.com>
2022-08-21 18:27:31 +0630

    Add files via upload

---
+++ Model/UserBean.java
@@ -0,0 +1,64 @@
+package Model;
+
+public class UserBean 
+{
+	private String username;
+	private String userid;
+	private String email;
+	private String password;
+	private String userrole;
+	private String searchid;
+	private String searchname;
+	private String confirmpassword;
+	
+	
+	public String getConfirmpassword() {
+		return confirmpassword;
+	}
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}
+	public String getSearchid() {
+		return searchid;
+	}
+	public void setSearchid(String searchid) {
+		this.searchid = searchid;
+	}
+	public String getSearchname() {
+		return searchname;
+	}
+	public void setSearchname(String searchname) {
+		this.searchname = searchname;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserrole() {
+		return userrole;
+	}
+	public void setUserrole(String userrole) {
+		this.userrole = userrole;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	
+}
