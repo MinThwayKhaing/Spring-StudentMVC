a5a1ecf5ad3c8b0c5ddbffd357beffe971dc1ac0
MinThwayKhaing <110376802+MinThwayKhaing@users.noreply.github.com>
2022-08-21 18:26:41 +0630

    Add files via upload

---
+++ DTO/CourseStudentResponseDTO.java
@@ -0,0 +1,19 @@
+package DTO;
+
+public class CourseStudentResponseDTO {
+	String coursename;
+	String stuid;
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	public String getStuid() {
+		return stuid;
+	}
+	public void setStuid(String stuid) {
+		this.stuid = stuid;
+	}
+	
+	}
---
+++ DTO/StudentRequestDTO.java
@@ -0,0 +1,83 @@
+package DTO;
+
+import java.util.List;
+
+public class StudentRequestDTO 
+{
+	private String studentid;
+	private String studentname;
+	private String dob;
+	private String gender;
+	private String phone;
+	private String education;
+	private List<String> course;
+	private String searchid;
+	private String searchname;
+	private String searchcourse;
+ 
+	public String getSearchid() {
+		return searchid;
+	}
+	public void setSearchid(String searchid) {
+		this.searchid = searchid;
+	}
+	public String getSearchname() {
+		return searchname;
+	}
+	public void setSearchname(String searchname) {
+		this.searchname = searchname;
+	}
+	public String getSearchcourse() {
+		return searchcourse;
+	}
+	public void setSearchcourse(String searchcourse) {
+		this.searchcourse = searchcourse;
+	}
+	public String getStudentid() {
+		return studentid;
+	}
+	public void setStudentid(String studentid) {
+		this.studentid = studentid;
+	}
+	public String getStudentname() {
+		return studentname;
+	}
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public List<String> getCourse() {
+		return course;
+	}
+	public void setCourse(List<String> course) {
+		this.course = course;
+	}
+	
+	
+	
+	
+	
+}
